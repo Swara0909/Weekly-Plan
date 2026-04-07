9ae5588643a2b95d8de030f42bee66314d70601e
Swara Nafade <swaranafade16@gmail.com>
2026-04-07 11:32:34 -0700

    updated

--- Week1/Day5/Conditional_prob.py
+++ Week1/Day5/Conditional_prob.py
@@ -10,11 +10,3 @@ prob=(subset['marks']>75).mean()
 print(prob)
 
 
-data=pd.DataFrame({
-    "spam":[1,1,0,1,0],
-    "link":[0,0,1,1,1]
-})
-
-spam=data[data["spam"]==1]
-prob_link_given_spam=(spam["link"]==1).mean()
-print(prob_link_given_spam)
